1155e81c86ce251b7d367e7d1df3c024fe388acb
Jung-Dahee <jungdahee0108@fasoo.com>
2024-03-28 12:05:52 +0900

    docs: cd 스크립트 및 설정 추가

---
+++ scripts/appStart.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+CURRENT_PID=$(pgrep -fla java | grep hayan | awk '{print $1}')
+
+kill -15 $CURRENT_PID
+
+JAR_NAME=$(ls -tr /home/ec2-user/dev/*SNAPSHOT.jar | tail -n 1)
+chmod +x $JAR_NAME
+nohup java -jar $JAR_NAME >> /home/ec2-user/dev/nohup.out 2>&1 &

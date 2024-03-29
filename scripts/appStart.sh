1964e7a725818a8c2212c95603b9b66512810038
Jung-Dahee <jungdahee0108@fasoo.com>
2024-03-29 10:50:08 +0900

    docs: appStart.sh 수정

--- scripts/appStart.sh
+++ scripts/appStart.sh
@@ -1,15 +1,8 @@
 #!/usr/bin/bash
 
-CURRENT_PID=$(pgrep -fla java | grep ci | awk '{print $1}')
+pkill -f  ci-cd-test.jar
 
-if [ -z "$CURRENT_PID" ]; then
-  echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
-else
-  echo "> kill -15 $CURRENT_PID"
-  kill -15 $CURRENT_PID
-  sleep 5
-fi
-
-JAR_NAME=$(ls -tr /home/ec2-user/dev/*.jar | tail -n 1)
+JAR_NAME=$(ls -tr /home/ec2-user/dev/ci-cd-test.jar | tail -n 1)
 chmod +x $JAR_NAME
+
 nohup java -jar $JAR_NAME >> /home/ec2-user/dev/nohup.out 2>&1 &

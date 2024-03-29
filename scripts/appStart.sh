49bc6efad1fa866f160ca2a64ec9a64806fd8072
Jung-Dahee <jungdahee0108@fasoo.com>
2024-03-29 14:55:30 +0900

    docs: appStart.sh 수정

--- scripts/appStart.sh
+++ scripts/appStart.sh
@@ -2,7 +2,4 @@
 
 pkill -f ci-cd-test.jar
 
-JAR_NAME=$(ls -tr /home/ec2-user/dev/ci-cd-test.jar | tail -n 1)
-chmod +x $JAR_NAME
-
-nohup java -jar $JAR_NAME >> /home/ec2-user/dev/nohup.out 2>&1 &
+nohup java -jar /home/ec2-user/dev/appStart.sh >> /home/ec2-user/dev/nohup.out 2>&1 &

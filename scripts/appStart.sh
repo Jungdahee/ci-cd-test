0be6ec2227279f44bb527b10e2f841a5f415eb29
Jung-Dahee <jungdahee0108@fasoo.com>
2024-03-29 10:22:10 +0900

    docs: appStart.sh 수정

--- scripts/appStart.sh
+++ scripts/appStart.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 
-CURRENT_PID=$(pgrep -fla java | grep hayan | awk '{print $1}')
+CURRENT_PID=$(pgrep -fla java | grep ci | awk '{print $1}')
 
 kill -15 $CURRENT_PID
 
-JAR_NAME=$(ls -tr /home/ec2-user/dev/*SNAPSHOT.jar | tail -n 1)
+JAR_NAME=$(ls -tr /home/ec2-user/dev/*.jar | tail -n 1)
 chmod +x $JAR_NAME
 nohup java -jar $JAR_NAME >> /home/ec2-user/dev/nohup.out 2>&1 &
